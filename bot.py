0db654f62e135422608d7d87ca1c1088bc5ce6e1
Steven Zeng <zengstevenz@gmail.com>
2021-02-22 09:27:40 -0500

    Comments added

--- bot.py
+++ bot.py
@@ -8,45 +8,59 @@ from selenium.common.exceptions import StaleElementReferenceException
 import time
 from time import sleep
 
+#Setup of the chrome driver and path
 PATH = "C:\Program Files (x86)\chromedriver_win32\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+
+
+#Navigate to the first site
 driver.get("https://pennstatecampusrec.org/")
 driver.implicitly_wait(5)
-signup = driver.find_element_by_link_text("Log In")
-signup.click()
+
+
+#Setup of future wait times
 wait = WebDriverWait(driver, 2) 
 wait1 = WebDriverWait(driver, 10)
 
 try:
+    #Begin Login Process native to site
+    signup = driver.find_element_by_link_text("Log In")
+    signup.click()
     element = wait.until(
         EC.presence_of_element_located((By.CLASS_NAME, "loginOption"))
     )
     element.click()
 
+    #Microsoft Login Page
     email = wait.until(
         EC.presence_of_element_located((By.ID, "i0116"))
     )
-    email.send_keys("")
+    email.send_keys("") #Enter your PSU e-mail here
     email.send_keys(Keys.RETURN)
 
-    psuUser = wait1.until(
+    #PSU Login Page
+    psuUser = wait.until(
         EC.presence_of_element_located((By.ID, "login"))
     )
-    psuUser.send_keys("")
+    psuUser.send_keys("") #Enter your e-mail here again
 
     psuPass = driver.find_element_by_id("password")
-    psuPass.send_keys("")
+    psuPass.send_keys("") #Enter your password here
     psuPass.send_keys(Keys.ENTER)
 
-    finalSignUp = wait.until(
+    finalSignUp = wait1.until(
         EC.presence_of_element_located((By.ID, "idSIButton9"))
     )
     finalSignUp.click()
+
+    #Navigation to Timeslot Register page
     driver.implicitly_wait(20)
-    #driver.get("https://pennstatecampusrec.org/Program/GetProgramDetails?courseId=115d1e23-c0bf-4e58-9185-e38e98a44e6b&semesterId=531292ec-80c2-458c-83f3-21eac12b2222")
+    #Test Case: driver.get("https://pennstatecampusrec.org/Program/GetProgramDetails?courseId=115d1e23-c0bf-4e58-9185-e38e98a44e6b&semesterId=531292ec-80c2-458c-83f3-21eac12b2222")
     driver.get("https://pennstatecampusrec.org/Program/GetProgramDetails?courseId=37c599b5-d91c-4111-ab0e-3a978a3d4d1c&semesterId=531292ec-80c2-458c-83f3-21eac12b2222")
 except:
     driver.quit()
+
+#Implement Refresh Loop until timeslot becomes available, which prompts a click
 link = None
 while not link:
     try:
@@ -54,19 +68,20 @@ while not link:
     except NoSuchElementException:
         driver.refresh()
 link.click()
+
+#Checkout Page, note: page javascript creates a stale element
 try:
     checkout = wait.until(
         EC.presence_of_element_located((By.ID, "checkoutButton"))
-    #id = checkoutButton
     )
     checkout.click()
 except StaleElementReferenceException:
     checkout = wait.until(
         EC.presence_of_element_located((By.ID, "checkoutButton"))
-    #id = checkoutButton
     )
     checkout.click()
 
+#Modal View, final checkout button clicked
 finalCheckout = wait.until(
     EC.presence_of_element_located((By.XPATH, "//div[@class='modal-footer']/button[@onclick='Submit()']"))
 )
